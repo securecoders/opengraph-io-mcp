a31a587b1f1a636ed04f6de1abeadae468e33106
cwooley <cwooley89@gmail.com>
2026-01-28 15:37:19 -0500

    remove unused packages, update other deps

--- src/tools/base.ts
+++ src/tools/base.ts
@@ -1,9 +1,6 @@
 import { z } from "zod";
-import { CallToolResult, Tool, ToolSchema } from "@modelcontextprotocol/sdk/types.js";
+import { CallToolResult, Tool } from "@modelcontextprotocol/sdk/types.js";
 import { zodToJsonSchema } from "zod-to-json-schema";
-
-const ToolInputSchema = ToolSchema.shape.inputSchema;
-type ToolInput = z.infer<typeof ToolInputSchema>;
 
 abstract class BaseTool {
     constructor() {}
@@ -20,7 +17,7 @@ abstract class BaseTool {
         return {
             name: this.name,
             description: this.description,
-            inputSchema: zodToJsonSchema(this.inputSchema) as ToolInput,
+            inputSchema: zodToJsonSchema(this.inputSchema) as Tool["inputSchema"],
         };
     }
 }
